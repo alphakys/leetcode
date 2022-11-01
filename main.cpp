eb7ba53e294b1a634683a4591f6a0f5746b7b0c4
alphakys <gys1120@naver.com>
2022-11-01 18:12:26 +0900

    first bad version

--- main.cpp
+++ main.cpp
@@ -6,37 +6,56 @@ class BinarySearch{
 public:
     int search(vector<int>& nums, int target) {
 
+        int len = nums.size();
+        int first = 0;
+        int end = len;
+        int pivot = (first+end)/2;
+        int answer = 0;
+
+        while(first <=pivot){
+            cout << "first : " << first << " pivot : " << pivot << " end : " << end << endl;
+            if(first==pivot && nums[pivot]!= target){answer = -1; break;}
+            if(nums[pivot] <target){
+                first = pivot+1;
+                pivot = (first + end)/2;
+            }else if(nums[pivot]== target){
+                answer = pivot;
+                break;
+            }else{
+                end = pivot;
+                pivot = (first+end)/2;
+            }
+
+        }
+        return answer;
     }
 };
 
 int main() {
-
-    vector<int> nums = {2,5};
-    int len = nums.size();
+    //10개
+    vector<int> nums = {0,0,0,1,1,1,1,1,1,1};
+    int n = 10;
     int first = 0;
-    int end = len;
-    int pivot = (first+end)/2;
-    int target = 2;
-    int answer = 0;
+    int end = n;
 
-    while(first <=pivot){
+    int pivot = 10;
+    int i = 0;
+    while(i<=10){
         cout << "first : " << first << " pivot : " << pivot << " end : " << end << endl;
-        if(first==pivot && nums[pivot]!= target){answer = -1; break;}
-        if(nums[pivot] <target){
-            first = pivot+1;
+
+        if(nums[pivot]==1){
+            end = pivot-1;
             pivot = (first + end)/2;
-        }else if(nums[pivot]== target){
-            answer = pivot;
-            break;
-        }else{
-            end = pivot;
-            pivot = (first+end)/2;
+        }
+        else{
+
         }
 
+        i++;
+
+
+
     }
-    cout << answer;
-//    auto *bs = new BinarySearch();
-//    bs->search(nums, 9);
 
 
 
