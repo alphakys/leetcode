45a38af085a8052dba18f83d80c449ba2efe2552
alphakys <gys1120@naver.com>
2022-12-25 22:26:24 +0900

    22.12.25

--- main.cpp
+++ main.cpp
@@ -1114,6 +1114,22 @@ class Tree{
             }
             return true;
 
+            // !!깔끔한 코드!!
+//            if(r1==nullptr && r2==nullptr)
+//                return true;
+//
+//            if(r1 != nullptr and r2 != nullptr){
+//
+//                return (
+//                        (r1->data == r2->data) &&
+//                        isIdentical(r1->left, r2->left)
+//                isIdentical(r1->right, r2->right)
+//
+//                )
+//            }
+//
+//            // one empty, one not -> false;
+//            return false;
         }
 
 
@@ -1124,6 +1140,7 @@ class Tree{
 
 
 };
+
 
 
 
@@ -1175,11 +1192,7 @@ void get_gcd(){
 
 int main() {
 
-    Node *a = nullptr;
-    Node *b = nullptr;
-
-    cout << (a and b);
-
+    cout << (true && false);
     return 0;
 }
 
